ca6d0c1cf3db25b523362d4cd47b647fecacaf68
Tarik <tarik.jaidani@gmail.com>
2025-09-12 21:07:32 +0200

    3

--- App.cpp
+++ App.cpp
@@ -2,43 +2,9 @@
 #include <string>
 using namespace std;
 
-enum enNumbertype { odd=1 , even=2};
+struct driverInfo {
+	int Age;
+	bool DriverLicence;
+};
 
-int readNumber()
-{
-	int Num;
-	cout << "Please input the number: " << endl;
-	cin >> Num;
-	return Num;
-}
-
-enNumbertype enNumberType(int Num)
-{
-	int result = Num % 2 ;
-	if (result == 0)
-	{
-		return enNumbertype::even;
-	}
-	else if (result == 1)
-	{
-		return enNumbertype::odd;
-	}
-}
-
-void printResult(enNumbertype NumType)
-{
-	if (NumType == enNumbertype::even)
-	{
-		cout << "Number is Even " << endl;
-	}
-	else if (NumType == enNumbertype::odd)
-	{
-		cout << "Number is Odd " << endl;
-	}
-}
-
-
-int main()
-{
-	printResult(enNumberType(readNumber()));
-}
+int 
