1d28a52f3d4042f93b079a916504a7ff90223499
Tarik <tarik.jaidani@gmail.com>
2025-09-23 00:13:50 +0200

    31

---
+++ Problem_29.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+enum enNumberType { odd = 1, even = 2 };
+
+
+int readInput()
+{
+	int input;
+	cout << "Write your Input: " << endl;
+	cin >> input;
+	return input;
+}
+
+enNumberType checkOddEven(int input)
+{
+	if (input % 2 == 0)
+		return enNumberType::even;
+	else
+		return enNumberType::odd;
+}
+
+
+int ForLoops(int input)
+{
+	int sum = 0;
+	for (int i = 1; i <= input; i++)
+		if (checkOddEven(i) == enNumberType::even)
+		{
+			sum += i;
+		};
+	return sum;
+}
+
+int doWhile(int input)
+{
+	int i = 1, sum = 0;
+	do
+	{
+		if (checkOddEven(i) == enNumberType::even)
+		{
+			sum += i;
+		};
+		i++;
+	} while (i <= input);
+
+
+	return sum;
+}
+
+int whileLoops(int input)
+{
+	int i = 1, sum = 0;
+	while (i <= input)
+	{
+		if (checkOddEven(i) == enNumberType::even)
+		{
+			sum += i;
+		};
+		i++;
+	}
+	return sum;
+}
+
+
+int main()
+{
+	int N = readInput();
+	cout << ForLoops(N) << endl;
+	cout << doWhile(N) << endl;
+	cout << whileLoops(N) << endl;
+}
---
+++ Problem_30.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int readN(string message)
+{
+	int N;
+	do {
+		cout << message << endl;
+		cin >> N;
+	} while (N < 0);
+
+	return N;
+}
+
+int sumNumbers(int N)
+{
+	int sum = 1;
+	for (int i = N; i >= 1; i--)
+	{
+		sum *= i;
+
+	}
+	return sum;
+}
+
+void printResult(int sum)
+{
+	cout << sum;
+}
+
+int main()
+{
+
+	printResult(sumNumbers(readN("Enter a number: ")));
+}
