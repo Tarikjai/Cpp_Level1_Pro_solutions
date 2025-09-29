2e33b14b3ba8ae0f2922d43f0adbbd6fd0666573
Tarik <tarik.jaidani@gmail.com>
2025-09-29 22:00:57 +0200

    41

---
+++ Problem_40.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+float ReadBillValue(string Message) {
+	float input;
+	cout << Message << endl;
+	cin >> input;
+	return input;
+}
+
+float CalculateCharge(float BillValue) {
+	float BillWitchCharges = 0;
+	return BillWitchCharges = BillValue * 1.10;
+}
+
+float CalculateSalesTax(float BillWitchCharges) {
+	float FinalBil;
+	return  FinalBil = BillWitchCharges * 1.16;
+}
+
+void printResult(float FinalBil) {
+	cout << FinalBil;
+}
+
+int main() {
+	float BillValue = ReadBillValue("Please enter the  BillValue : ");
+
+	printResult(CalculateSalesTax(CalculateCharge(BillValue)));
+
+}
+
