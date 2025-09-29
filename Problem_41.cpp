f4652d7a5829c08c415135c4db44622a9ea32cf2
Tarik <tarik.jaidani@gmail.com>
2025-09-29 23:01:50 +0200

    41

--- Problem_41.cpp
+++ Problem_41.cpp
@@ -1,3 +1,49 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+float ReadHours(string Message) {
+	float Number;
+	do {
+		cout << Message << endl;
+		cin >> Number;
+		if (Number <= 0) {
+			cout << "Number must be > 0! " << endl;
+			cout << endl;
+		}
+	} while (Number <= 0);
+	return Number;
+}
+
+
+float HoursToDays(float NumberHours) {
+
+	return float(NumberHours / 24);
+
+}
+
+float HoursToWeeks(float NumberHours) {
+	return float(NumberHours / 24 / 7);
+}
+
+float DaysToWeeks(float days) {
+	;
+	return   float(days) / 7;
+
+}
+
+
+int main() {
+	float NumberHours = ReadHours("Please input the number of hours: ");
+
+	float NumberOfdays = HoursToDays(NumberHours);
+	float NumberOfweeks = DaysToWeeks(NumberOfdays);
+
+	cout << "Total hours " << NumberHours << endl;
+	cout << endl;
+	cout << "weeks: " << NumberOfweeks << "   " << HoursToWeeks(NumberHours);
+	cout << endl;
+	cout << "Days: " << NumberOfdays << endl;
+	cout << endl;
+}
+
