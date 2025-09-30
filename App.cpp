43b3421c6cb1b2329118e97eac9effe2d7164058
Tarik <tarik.jaidani@gmail.com>
2025-09-30 20:41:48 +0200

    41

--- App.cpp
+++ App.cpp
@@ -2,48 +2,15 @@
 #include <string>
 using namespace std;
 
-float ReadHours(string Message) {
-	float Number;
-	do {
-		cout << Message << endl;
-		cin >> Number;
-		if (Number <= 0) {
-			cout << "Number must be > 0! " << endl;
-			cout << endl;
-		}
-	} while (Number <= 0);
-	return Number;
-}
+struct TaskDuration {
+    float days;
+    float hours;
+    float minutes;
+    float seconds;
+};
 
-
-float HoursToDays(float NumberHours) {
-	 
-	return float(NumberHours / 24);
-	
-}
-
-float HoursToWeeks(float NumberHours) {
-	return float(NumberHours / 24 / 7);
-}
-
-float DaysToWeeks(float days) {
-	 ;
-	return   float(days) / 7;
-	 
-}
- 
 
 int main() {
-	float NumberHours = ReadHours("Please input the number of hours: ");
-
-	float NumberOfdays = HoursToDays(NumberHours);
-	float NumberOfweeks = DaysToWeeks(NumberOfdays);
-
-	cout << "Total hours " << NumberHours << endl;
-	cout << endl;
-	cout << "weeks: " << NumberOfweeks << "   "  << HoursToWeeks(NumberHours);
-	cout <<   endl;
-	cout << "Days: " << NumberOfdays << endl;
-	cout << endl;
+	 
 }
 
