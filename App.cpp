0a6d85953d9e2799af9c87b5241378003766984d
Tarik <tarik.jaidani@gmail.com>
2025-10-02 00:05:25 +0200

    41

--- App.cpp
+++ App.cpp
@@ -11,14 +11,28 @@ struct TaskDuration {
 
 TaskDuration readInput() {
     TaskDuration Duration;
-    cout << "" << endl;
+    cout << "days" << endl;
     cin >> Duration.days;
+    cout << "hours" << endl;
+    cin >> Duration.hours;
+    cout << "minutes" << endl;
+    cin >> Duration.minutes;
+    cout << "seconds" << endl;
+    cin >> Duration.seconds;
+    return Duration;
 }
+
+
+float calculateDaystoSeconds(TaskDuration Duration) {
+  
+    return  float DaystoSeconds = Duration.days * 24 * 60 * 60;
+}
+
 
 
 int main() {
 	 
-
+     
 
 }
 
