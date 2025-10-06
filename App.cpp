1a9b8fd894a9fc19e54d4ca0b75a794ab6b5783d
Tarik <tarik.jaidani@gmail.com>
2025-10-06 23:51:18 +0200

    01

--- App.cpp
+++ App.cpp
@@ -2,44 +2,13 @@
 #include <string>
 using namespace std;
 
-string ReadPin() {
-	string PIN;
-	cout << "Insert Your PIN" << endl;
-	cin >> PIN;
-	return PIN;
+void PrintName(string Name) {
+	cout << Name << endl;
 }
 
-bool Login() {
-	string PIN;
-	int counter = 3;
-
-	do {
-		counter--;
-		PIN = ReadPin();
-		if (PIN == "1234")
-		{
-			return 1;
-		}
-		else {
-			system("color 4F");
-			cout << "Wrong PIN, you have "<< counter << " more tries" << endl;
-			cout << endl;
-		 
-		}
-
-	} while (counter >= 1 && PIN != "1234");
-	return 0;
-}
 
 int main() {
-
-	if (Login()) {
-		system("color 2F");
-		cout << "Your Balance is: 7500" << endl;
-	}
-	else {
-		cout << "Card is locked" << endl;
-	}
-
+	string Name;
+	PrintName("Tarik");
 
 }
--- Problem_5.cpp
+++ Problem_5.cpp
@@ -2,7 +2,6 @@
 #include <string>
 using namespace std;
 
-
 #include <iostream>
 #include <string>
 using namespace std;
