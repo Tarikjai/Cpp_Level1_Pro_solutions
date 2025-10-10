5c69fb9d9ba5ac4035f2f515bf9c7a7b35d8c1c0
Tarik <tarik.jaidani@gmail.com>
2025-10-10 23:31:22 +0200

    055

---
+++ Problem_05.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct personInfo {
+	int Age;
+	bool HasDriverLicense;
+	bool HasRecommendation;
+};
+
+
+personInfo readInfo()
+{
+	personInfo Info;
+	cout << "Please type your age: " << endl;
+	cin >> Info.Age;
+	cout << "Type 1 if you have a driver license, 0 if not: " << endl;
+	cin >> Info.HasDriverLicense;
+	cout << "Do you have recommendation? " << endl;
+	cin >> Info.HasRecommendation;
+	return Info;
+}
+
+bool isAccepted(personInfo Info)
+{
+	if (Info.HasRecommendation)
+	{
+		return true;
+	}
+	else
+	{
+		return(Info.Age > 21 && Info.HasDriverLicense);
+	}
+
+}
+
+void printResult(personInfo Info)
+{
+	if (isAccepted(Info))
+	{
+		cout << "Hired";
+	}
+	else
+	{
+		cout << "Rejected";
+	}
+}
+
+
+int main()
+{
+	printResult(readInfo());
+}
