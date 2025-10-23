c02020a551dda36338ef93bf84f367d33a53e8a3
Tarik <tarik.jaidani@gmail.com>
2025-10-24 00:00:08 +0200

    47

--- App.cpp
+++ App.cpp
@@ -3,7 +3,28 @@
 #include <string>
 using namespace std;
 
+ 
+float ReadInput(string Message) {
+	float input=0;
+	do {
+		cout << Message << endl;
+		cin >> input;
+	} while (input <= 0);
+	return input;
+};
+
+float calculate(float LA, float MP) {
+	return LA / MP;
+}
+
+void PrintResult(float result) {
+	cout << result << " Months";
+}
 
 int main() {
- 
+
+	float LA = ReadInput("Please Enter the Loan Amount");
+	float MP = ReadInput("Please Enter the Monthly Payment");
+
+	PrintResult(calculate(LA, MP));
 }
--- Problem_046.cpp
+++ Problem_046.cpp
@@ -2,3 +2,15 @@
 #include <ctime>
 #include <string>
 using namespace std;
+
+void printAZ() {
+	for (int i = 65; i <= 90; i++) {
+		cout << char(i) << endl;
+	}
+}
+
+
+int main() {
+	printAZ();
+}
+
